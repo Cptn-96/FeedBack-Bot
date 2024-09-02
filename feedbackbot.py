d943d3e78d37ddf3af8a5c9ec09ed89351d76a30
Cptn-96 <133875638+Cptn-96@users.noreply.github.com>
2024-09-02 23:33:34 +0300

    Update feedbackbot.py

--- feedbackbot.py
+++ feedbackbot.py
@@ -1,6 +1,6 @@
 import telebot
 from telebot import *
-bot_token = "7135431999:AAEigm-7Rnl5hrd5lpoy_U_QeSfI11S2aXY"
+bot_token = "bot token"
 bot = telebot.TeleBot(bot_token)
 
 @bot.message_handler(commands=['start'])
@@ -8,18 +8,17 @@ def welcome_user(message):
     markup = types.ReplyKeyboardMarkup(row_width=1)
 
     item2 = types.KeyboardButton('Questions')
-    item3 = types.KeyboardButton('Registration link')
-    markup.add(item2, item3)
-    bot.reply_to(message, "Aselamu'aleykum werahmetullahi weberekathu.\nThis is code minarets group bot. Feel free to give us any feedback, questions, comments.", reply_markup=markup)
+    markup.add(item2)
+    bot.reply_to(message, "Feel free to give us any feedback, questions, comments.", reply_markup=markup)
 
 @bot.message_handler(func=lambda message: True)
 def user_message(message):
-    Owner_id = "5534731484"
+    Owner_id = "ownerid" #replace it with your id
     receiver = message.chat.id
     text = message.text
     chat_member = bot.get_chat_member(message.chat.id, message.chat.id )
     username = chat_member.user.username
-    if message.chat.id == 5534731484:
+    if message.chat.id == Owner_id: 
         if text[0:2] == "//":
             userid = int(text[2:12])
             bot.send_message(userid, text[13:])
@@ -29,9 +28,6 @@ def user_message(message):
 
     elif text == 'Questions':
         bot.send_message(receiver, "Feel free to ask anything you want.")
-
-    elif text ==  "Registration link":
-        bot.send_message(receiver, "Here is the link to our channel: \n https://forms.gle/cEiMVK3D12uiJVFo7 ")
 
     else:
         bot.send_message(Owner_id,f"Message from @{username} id: {receiver}: \n-{text}")
